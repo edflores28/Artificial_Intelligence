2e7e9b340406efd4bbade17d2bedddf4f6133666
Edwin Flores <edflores28@gmail.com>
2019-02-08 17:19:52 -0500

    Module2 initial commit

---
+++ Module 2/module02.py
@@ -0,0 +1,24 @@
+import push
+
+### YOUR SOLUTION HERE ###
+### YOUR SOLUTION HERE ###
+
+def minimax_versus_random():
+    ### YOUR SOLUTION HERE ###
+    # Refer to the PDF for an example of the output this function should print.
+    ### YOUR SOLUTION HERE ###
+    pass # remove
+
+
+def minimax_versus_alphabeta():
+    ### YOUR SOLUTION HERE ###
+    # Refer to the PDF for an example of the output this function should print.
+    ### YOUR SOLUTION HERE ### 
+    pass # remove
+
+
+if __name__ == "__main__":
+    print("Random v. Minimax")
+    minimax_versus_random()
+    print("\nMinimax v. Alpha Beta")
+    minimax_versus_alphabeta()
