e0ae9efc370535c0993f7ea4d59339c974590df9
Edwin Flores <edflores28@gmail.com>
2019-04-03 11:35:22 +0900

    Initial module 10

---
+++ Module 10/module10.py
@@ -0,0 +1,87 @@
+import sys
+import random
+
+clean_data = {
+    "plains": [
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, "plains"]
+    ],
+    "forest": [
+        [0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, "forest"],
+        [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, "forest"],
+        [1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, "forest"],
+        [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, "forest"]
+    ],
+    "hills": [
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, "hills"],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, "hills"],
+        [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, "hills"],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, "hills"]
+    ],
+    "swamp": [
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, "swamp"],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, "swamp"]        
+    ]
+}
+
+def blur(data):
+    def apply_noise(value):
+        if value < 0.5:
+            v = random.gauss(0.10, 0.05)
+            if v < 0.0:
+                return 0.0
+            if v > 0.75:
+                return 0.75
+            return v
+        else:
+            v = random.gauss(0.90, 0.10)
+            if v < 0.25:
+                return 0.25
+            if v > 1.00:
+                return 1.00
+            return v
+    noisy_readings = [apply_noise(v) for v in data[0:-1]]
+    return noisy_readings + [data[-1]]
+
+def generate_data(clean_data, total_per_label):
+    labels = len(clean_data.keys())
+    def one_hot(n, i):
+        result = [0.0] * n
+        result[i] = 1.0
+        return result
+
+    data = []
+    for i, label in enumerate(clean_data.keys()):
+        for _ in range(total_per_label):
+            datum = blur(random.choice(clean_data[label]))
+            xs = datum[0:-1]
+            ys = one_hot(labels, i)
+            data.append((xs, ys))
+    random.shuffle(data)
+    return data
+
+def learn_model(data, n_hidden, debug=False):
+    for x in data:
+        print(x)
+    print(len(data), data[10])
+    return [],[]
+
+def apply_model(model, data, labeled=False):
+    return []
+
+def evaluate_results(results):
+    return 1.00 # 100% error rate
+
+if __name__ == "__main__":
+    debug = len(sys.argv) > 1 and sys.argv[1].lower() == 'debug'
+
+    train_data = generate_data(clean_data, 10)
+    model = learn_model(train_data, 4, debug)
+
+    test_data = generate_data(clean_data, 100)
+    results = apply_model(model, test_data)
+
+    for result in results[0:10]:
+        print(result)
+
+    error_rate = evaluate_results(results)
+    print(f"The error rate is {error_rate}")
